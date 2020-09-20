6419e04e282fe207e3f72f1689607479f88b8798
Shah Rahman <shahmwr@gmail.com>
2020-09-20 09:52:25 -0500

    update value extraction from form

--- word-finder.js
+++ word-finder.js
@@ -1,24 +1,28 @@
 // Dictionary word matching 
 // a.k.a 'Yo dawg, I heard you like words. So I got you some other words that have your word in it so you can word while you word!'
 
-
-
-
-function output() {
-    const word = document.getElementById('word').value
-    const fullWord = document.getElementById('full-word').value
-    const allWord = document.getElementById('all-word').value
-    console.log("The form was submitted");
-    console.log(word);
-    console.log(fullWord);
-    console.log(allWord);
-  }
-
-
 // Fetch the text file containing dictionary terms
 fetch('engmix.txt')
-  .then(response => response.text())
-  .then(data => {
-  	// Let's take the user's input and find a match.
-  	//console.log(data);
-  });
+    .then(response => response.text())
+    .then(data => {
+        // Let's take the user's input and find a match.
+        //console.log(data);
+    });
+
+function output() {
+    console.log("The form was submitted");
+    // Get the word from user input
+    const word = document.getElementById('word').value
+        // Find full user input wod only
+    var fullWord = document.getElementById('full-word').checked
+        // Find all words containing user input
+    var allWord = document.getElementById('all-word').checked
+
+    /* console.log(word);
+    console.log(fullWord);
+    console.log(allWord);
+    */
+
+
+
+}
