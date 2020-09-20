e0cc4b3334084535aea2a77435675b026185672e
Cyclicentropy <69284925+Cyclicentropy@users.noreply.github.com>
2020-09-20 12:25:26 -0500

    Finish result processing for word finding

--- word-finder.js
+++ word-finder.js
@@ -2,12 +2,21 @@
 // a.k.a 'Yo dawg, I heard you like words. So I got you some other words that have your word in it so you can word while you word!'
 
 // Fetch the text file containing dictionary terms
+/* let dictionaryData;
 fetch('engmix.txt')
     .then(response => response.text())
     .then(data => {
         // Let's take the user's input and find a match.
-        //console.log(data);
-    });
+        dictionaryData = response;
+    }); */
+
+/* let dictionaryData;
+fetch('engmix.txt')
+.then(response => response.text())
+.then(data => console.log(data));
+ */
+
+const matches = document.getElementById('matches')
 
 function output() {
     console.log("The form was submitted");
@@ -18,11 +27,44 @@ function output() {
         // Find all words containing user input
     var allWord = document.getElementById('all-word').checked
 
-    /* console.log(word);
+    console.log(word);
     console.log(fullWord);
     console.log(allWord);
-    */
 
+    /* async function getDataset() {
+      let response = await fetch('engmix.txt');
+      let theData = await response.text(response);
+      return console.log(theData); 
+    }
+    getDataset(); */
 
+    function fetchData() {
+      return fetch('engmix.txt')
+              .then(response =>
+                  response.text().then(text => text.split(/\r|\n/)));
+  }
+  
+  fetchData().then(arr => {
+    if(arr.includes(word)){
+      console.log("Found a match");
+      const matches = arr.filter(s => s.includes(word));
+      console.log(matches);
+      let listView = matches.join('<li>' + '</li>');
+      console.log(listView);   
+      if(fullWord == true){
+        console.log(fullWord);
+        document.getElementById("matches").innerHTML = word;
+      } else if(allWord == true){
+        console.log(allWord);
+        document.getElementById("matches").innerHTML = listView;
+      }
+      
+    }
+  }
+    
 
+    
+    );
+    
 }
+
